9fd10c11605a5dd47f05b1d78c1b9d1ca3b320a0
seroqn <sero.qn@gmail.com>
2022-07-17 21:47:01 +0900

    rfc petit

--- src/deps.ts
+++ src/deps.ts
@@ -2,8 +2,6 @@ export { Command } from "https://deno.land/x/cliffy@v0.24.2/command/mod.ts";
 export { colors } from "https://deno.land/x/cliffy@v0.24.2/ansi/colors.ts";
 import xdg from "https://deno.land/x/xdg@v9.4.0/src/mod.deno.ts";
 export { xdg };
-//export {$} from 'https://deno.land/x/zx_deno@1.2.2/mod.mjs'
-//export { exec } from "https://deno.land/x/exec@0.0.5/mod.ts";
 
 export { sprintf } from "https://deno.land/std@0.144.0/fmt/printf.ts";
 
@@ -13,4 +11,3 @@ export {
 } from "https://deno.land/std@0.144.0/fs/mod.ts";
 export * as path from "https://deno.land/std@0.144.0/path/mod.ts";
 export { parse as parseYaml } from "https://deno.land/std@0.144.0/encoding/yaml.ts";
-//export { parse as parseYaml } from "https://deno.land/x/yaml@v2.1.1/src/index.ts";

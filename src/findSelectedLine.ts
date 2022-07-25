da58209930b38c4e46357618fa4a6f914f71716e
seroqn <sero.qn@gmail.com>
2022-07-25 12:46:08 +0900

    fix findSelectedLine.ts

--- src/findSelectedLine.ts
+++ src/findSelectedLine.ts
@@ -3,13 +3,16 @@ import { isReadable } from "./mod.ts";
 import { escapeStringRegexp } from "./deps.ts";
 
 function _splitQjhead(qjhead: string) {
+  if (qjhead == "") {
+    return [];
+  }
   const heads = [];
   for (
     const ms of qjhead.matchAll(/(?:^|\.\.)(?:"(.*?)"|(.*?))(?=(?:\.\.|$))/g)
   ) {
     const head = ms[1] ?? ms[2] ?? "";
     if (!head) {
-      throw new Error(`head parse error: '${qjhead}'`);
+      throw new Error(`head parse error: "${qjhead}"`);
     }
     heads.push(head);
   }
@@ -30,37 +33,46 @@ function _nextIndent(lines: string[], i: number, indent: number) {
   }
   return -1;
 }
-function _findHeadRow(heads: string[], lines: string[]) {
+function _findHeadRow(
+  lines: string[],
+  heads: string[],
+  row: number,
+  indent: number,
+) {
   let h = 0, hlen = heads.length;
-  let indent = _nextIndent(lines, 0, -1 + 1);
+  indent = _nextIndent(lines, row, indent);
   if (indent == -1) {
-    return -1;
+    return [-1, -1];
   }
-  let termRe = new RegExp(String.raw`^\s{${indent},}\S|^\s*$`);
+  let stopRe = new RegExp(String.raw`^\s{${indent},}\S|^\s*$`);
   let headRe = new RegExp(
-    String.raw`^\s{${indent}}(["']?)${escapeStringRegexp(heads[h])}\1:`,
+    String.raw`^\s{${indent}}(["']?)(?:\+\s*)?${
+      escapeStringRegexp(heads[h])
+    }\1:`,
   );
-  for (let i = 0, len = lines.length; i < len; i++) {
+  for (let i = row, len = lines.length; i < len; i++) {
     const line = lines[i];
     if (/^\s*#/.test(line)) {
       continue;
-    } else if (!termRe.test(line)) {
+    } else if (!stopRe.test(line)) {
       break;
     } else if (!headRe.test(line)) {
       continue;
     } else if (++h >= hlen) {
-      return i;
+      return [i, indent];
     }
     indent = _nextIndent(lines, i + 1, indent + 1);
     if (indent == -1) {
       break;
     }
-    termRe = new RegExp(String.raw`^\s{${indent},}\S|^\s*$`);
+    stopRe = new RegExp(String.raw`^\s{${indent},}\S|^\s*$`);
     headRe = new RegExp(
-      String.raw`^\s{${indent}}(["']?)${escapeStringRegexp(heads[h])}\1:`,
+      String.raw`^\s{${indent}}(["']?)(?:\+\s*)?${
+        escapeStringRegexp(heads[h])
+      }\1:`,
     );
   }
-  return -1;
+  return [-1, -1];
 }
 
 if (Deno.args.length != 1) {
@@ -72,16 +84,27 @@ if (!isReadable(path)) {
   console.error(`file is not readable: "${path}"`);
   Deno.exit();
 }
-const [_, jhead, article] = Deno.args[0].split("\t");
+const [_, jhead, theme, article] = Deno.args[0].split("\t");
+
 const qjhead = jhead.trim().slice(1, -1); // remove `[` `] `
 const heads = _splitQjhead(qjhead);
+const qtheme = theme.trim();
+const themes = _splitQjhead(qtheme);
 if (!heads.length) {
   console.error(`invalid heads: '${jhead}'`);
   Deno.exit();
 }
 const lines = Deno.readTextFileSync(path).split("\n");
-const row = _findHeadRow(heads, lines);
+let [row, indent] = _findHeadRow(lines, heads, 0, 0);
+if (row != -1 && themes.length) {
+  const [r, i] = _findHeadRow(lines, themes, row + 1, indent);
+  if (r == -1) {
+    console.error(`fail in finding theme`, themes);
+  }
+  row = r == -1 ? row : r;
+}
 
+// output: (path, row, searchWord)
 console.log(path);
 console.log(String(row + 1)); // 文字列化しないと ansi-color 付きの数字を出力して bash がエラーを出す
 console.log(heads.slice(-1)[0]);
